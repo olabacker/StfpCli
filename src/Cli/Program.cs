dce5573b8adceda3fb3cf22eafaa6eca681bdd07
ola <ola.backer@snapcode.se>
2021-04-13 09:43:37 +0200

    fix broken file structure

---
+++ src/Cli/Program.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console.Cli;
+using System;
+
+namespace SftpCli
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new CommandApp();
+
+            app.Configure(config =>
+            {
+                config.AddCommand<UploadCommand>("upload");
+
+                config.AddCommand<DownloadCommand>("download");
+            });
+
+            return app.Run(args);
+        }
+    }
+}
---
+++ src/Cli/Settings.cs
@@ -0,0 +1,27 @@
+﻿using Spectre.Console.Cli;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SftpCli
+{
+    public class SftpSettings : CommandSettings
+    {
+        [CommandArgument(0, "<IpAddress>")]
+        public string IpAddress { get; set; }
+
+        [CommandArgument(2, "<User>")]
+        public string User { get; set; }
+
+        [CommandArgument(3, "<Pass>")]
+        public string Password { get; set; }
+
+        [CommandArgument(4, "<LocalPath>")]
+        public string LocalPath { get; set; }
+
+        [CommandArgument(5, "<LocalPath>")]
+        public string RemotePath { get; set; }
+    }
+}
